3dd8973d7dd7048b063d453f27d967bd6494985c
Riacule <sophie.earl@live.com>
2023-01-23 01:17:02 +0000

    bug fix

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,5 @@
 import preprocess from 'svelte-preprocess';
-import adapter from '@sveltejs/adapter-auto';
+
 import { vitePreprocess } from '@sveltejs/kit/vite';
 import adapter from '@sveltejs/adapter-static';
 
@@ -22,11 +22,7 @@ const config = {
 			assets: 'build',
 			fallback: null,
 			precompress: false
-		}),
-		prerender: {
-			// This can be false if you're using a fallback (i.e. SPA mode)
-			default: true
-		}
+		})
 	}
 };
 
